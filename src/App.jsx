bc9e2a0438dd9b6c20b515a1bd0c640b8fac3478
Nick Bowden <bowdenn@oregonstate.edu>
2023-03-16 00:38:38 -0700

    integrated game elements, user inputs, added pokemon image to main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ImagePixelated } from "react-pixelate"
 import './App.css'
 import Pokedex from './components/Pokedex';
 
 function App() {
+  const pixels = [30, 20, 15, 10, 5]
 
   const [pokemon, setPokemon] = useState({});
 
+  const [gameState, setGameState] = useState('');
+
+  const [guess, setGuess] = useState('');
+  const [pixelDensity, setPixelDensity] = useState(0);
+
   async function getPokemon() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/150');
+    const id = Math.floor(Math.random() * 152) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const newPokemon = await response.json();
     setPokemon(newPokemon)
   }
 
-  getPokemon();
+  function checkGuess() {
+    if (gameState === 'win' || gameState === 'lose') resetGame();
+
+    if (guess.toLowerCase() === pokemon.name) {
+      setGameState('win')
+      return
+    }
+
+    if (pixelDensity === 4) {
+      setGameState('lose')
+      return
+    }
+
+    setPixelDensity(pixelDensity + 1)
+
+  }
+
+  async function resetGame() {
+    await getPokemon();
+    setGameState('');
+    setGuess('');
+    setPixelDensity(0);
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, [])
 
   return (
     <div className="App">
-      <Pokedex />
+      <Pokedex image={pokemon.sprites?.front_default} name={pokemon.name? pokemon.name: ''} guess={guess} setGuess={setGuess} pixels={gameState === ''? pixels[pixelDensity]: 0} handleCheckGuess={()=> checkGuess()} gameState={gameState} pixelDensity={pixelDensity}/>
     </div>
   )
 }
--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,7 +1,18 @@
 import './Pokedex.css'
 import React from 'react';
+import { ImagePixelated } from "react-pixelate"
 
-function Pokedex() {
+function Pokedex({image, name, guess, setGuess, pixels, handleCheckGuess, gameState, pixelDensity}) {
+    function checkEnter(e) {
+        if (e.key === 'Enter') { 
+            handleEnter();
+        }
+    }
+
+    function handleEnter() {
+        handleCheckGuess();
+        setGuess('')
+    }
     return (
         <div className='pokedex'>
             <div className="pokedex-left">
@@ -32,22 +43,28 @@ function Pokedex() {
                     />
                 </svg>
                 <div className="interface">
-                    <div className="main-screen-outer">
+                    <div className="main-screen-outer white">
                         <div className="top-screen-buttons">
-                            <div className="top-button"></div>
-                            <div className="top-button"></div>
+                            <div className={`top-button ${gameState === ''? 'red': 'gray'}`}></div>
+                            <div className={`top-button ${pixelDensity <= 3? 'red': 'gray'}`}></div>
+                            <div className={`top-button ${pixelDensity <= 2? 'red': 'gray'}`}></div>
+                            <div className={`top-button ${pixelDensity <= 1? 'red': 'gray'}`}></div>
+                            <div className={`top-button ${pixelDensity <= 0? 'red': 'gray'}`}></div>
                         </div>
                         <div className="main-screen">
-                            <div className="screen"></div>
+                            <div className="screen">
+                            {/* #98cb98 */}
+                                <ImagePixelated src={image} width={224} height={224} centered={true} pixelSize={pixels} fillTransparencyColor={"#98cb98"}/>
+                            </div>
                         </div>
                         <div className="bottom-screen-buttons">
-                            <div className="bottom-button">
+                            <div className="bottom-button red">
                                 <div className="bottom-button-highlight"></div>
                             </div>
                             <div className="bottom-speaker">
-                                <div class="bar1"></div>
-                                <div class="bar2"></div>
-                                <div class="bar3"></div>
+                                <div className="bar1"></div>
+                                <div className="bar2"></div>
+                                <div className="bar3"></div>
                             </div>
                         </div>
                     </div>
@@ -55,25 +72,27 @@ function Pokedex() {
                     <div className="main-buttons">
                         <div className="start-select">
                             <div className="thin-button red"></div>
-                            <div className="thin-button blue"></div>
+                            <div className="thin-button periwinkle"></div>
                         </div>
 
                         <div className="sticks">
                             <div className="sticks-left">
-                                <div className="round-stick"></div>
+                                <div className="round-stick gray"></div>
                                 <div className="left-tiny-buttons">
                                     <div className="tiny-button"></div>
                                     <div className="tiny-button"></div>
                                 </div>
                             </div>
-                            <div className="sticks-center"></div>
+                            <div className="rectangle-button mint-green">
+                                {gameState === ''? '????': name.charAt(0).toUpperCase() + name.slice(1)}
+                            </div>
                             <div className="sticks-right">
-                                <div class="nav-direction">
-                                    <div class="nav-center-circle"></div>
-                                    <div class="nav-button-vertical"></div>
-                                    <div class="nav-button-horizontal">
-                                        <div class="border-top"></div>
-                                        <div class="border-bottom"></div>
+                                <div className="nav-direction">
+                                    <div className="nav-center-circle"></div>
+                                    <div className="nav-button-vertical"></div>
+                                    <div className="nav-button-horizontal">
+                                        <div className="border-top"></div>
+                                        <div className="border-bottom"></div>
                                     </div>
                                 </div>
                                 <div className="bottom-right">
@@ -94,7 +113,7 @@ function Pokedex() {
             </div>
 
             <div className="pokedex-right">
-                <div class="empty-container">
+                <div className="empty-container">
                     <svg height="125px" width="400px">
                         <polyline
                         points="0,0 0,71 278,71 298,118 400,118 400,0 0,0"
@@ -108,18 +127,21 @@ function Pokedex() {
                     </svg>
                 </div>
                 <div className="interface">
-                    <div className="secondary-screen"></div>
+                    <div className="secondary-screen" >
+                        <input type="text" spellCheck="false" className="guess-input" value={guess} placeholder={gameState === ''? 'Guess Name': `You ${gameState}`} onChange={(e) => setGuess(e.target.value)} onKeyUp={(e)=>checkEnter(e)} disabled={gameState !== ''} maxLength="12"/>
+                        <button className="guess-enter" onClick={handleEnter}>{gameState === ''? 'Enter': 'Replay?'}</button>
+                    </div>
                     <div className="gen-buttons">
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
-                        <button className="square-button blue"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
+                        <button className="square-button periwinkle"></button>
                     </div>
                     <div className="right-thin-buttons">
                         <div className="square-thin-button gray"></div>
@@ -137,8 +159,8 @@ function Pokedex() {
                                 </div>                                
                             </div>
                             <div className="stacked-squares">
-                                <div className="square-button white"></div>
-                                <div className="square-button white"></div>
+                                <button className="white-square"></button>
+                                <button className="white-square"></button>
                             </div>
                         </div>
                         <div className="large-round-button yellow">
@@ -147,8 +169,8 @@ function Pokedex() {
                     </div>
 
                     <div className="right-bottom">
-                        <div className="sticks-center"></div>
-                        <div className="sticks-center"></div>
+                        <div className="rectangle-button gray"></div>
+                        <div className="rectangle-button gray"></div>
                     </div>
                 </div>
             </div>
