4e2f44ebe771e2e30e983e8d6fdc5c12b16be0a3
Nick Bowden <bowdenn@oregonstate.edu>
2023-03-15 17:26:33 -0700

    left side of pokedex finished for now

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -7,7 +7,7 @@ function Pokedex() {
             <div className="pokedex-left">
                 <div className="top-buttons">
                     <div className="power-outer">
-                        <div className="power-inner">
+                        <div className="power-inner blue">
                             <div className="power-highlight"></div>
                         </div>
                     </div>
@@ -33,9 +33,63 @@ function Pokedex() {
                 </svg>
                 <div className="interface">
                     <div className="main-screen-outer">
-                    
+                        <div className="top-screen-buttons">
+                            <div className="top-button"></div>
+                            <div className="top-button"></div>
+                        </div>
+                        <div className="main-screen">
+                            <div className="screen"></div>
+                        </div>
+                        <div className="bottom-screen-buttons">
+                            <div className="bottom-button">
+                                <div className="bottom-button-highlight"></div>
+                            </div>
+                            <div className="bottom-speaker">
+                                <div class="bar1"></div>
+                                <div class="bar2"></div>
+                                <div class="bar3"></div>
+                            </div>
+                        </div>
                     </div>
 
+                    <div className="main-buttons">
+                        <div className="start-select">
+                            <div className="thin-button red"></div>
+                            <div className="thin-button blue"></div>
+                        </div>
+
+                        <div className="sticks">
+                            <div className="sticks-left">
+                                <div className="round-stick"></div>
+                                <div className="left-tiny-buttons">
+                                    <div className="tiny-button"></div>
+                                    <div className="tiny-button"></div>
+                                </div>
+                            </div>
+                            <div className="sticks-center"></div>
+                            <div className="sticks-right">
+                                <div class="nav-direction">
+                                    <div class="nav-center-circle"></div>
+                                    <div class="nav-button-vertical"></div>
+                                    <div class="nav-button-horizontal">
+                                        <div class="border-top"></div>
+                                        <div class="border-bottom"></div>
+                                    </div>
+                                </div>
+                                <div className="bottom-right">
+                                    <div className="med-button red">
+                                        <div className="med-button-highlight"></div>
+                                    </div>
+                                    <div className="right-tiny-buttons">
+
+                                        <div className="tiny-button"></div>
+                                        <div className="tiny-button"></div>
+                                        <div className="tiny-button"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
